606a30f010f2825f8551a3fcf35648386d921a97
veenu-wootz <vinay.jadon@wootz.work>
2025-02-20 17:32:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
-/*
-Copyright 2019 Adobe
-All Rights Reserved.
+// /*
+// Copyright 2019 Adobe
+// All Rights Reserved.
 
-NOTICE: Adobe permits you to use, modify, and distribute this file in
-accordance with the terms of the Adobe license agreement accompanying
-it. If you have received this file from a source other than Adobe,
-then your use, modification, or distribution of it requires the prior
-written permission of Adobe.
-*/
+// NOTICE: Adobe permits you to use, modify, and distribute this file in
+// accordance with the terms of the Adobe license agreement accompanying
+// it. If you have received this file from a source other than Adobe,
+// then your use, modification, or distribution of it requires the prior
+// written permission of Adobe.
+// */
 
-/* Control the default view mode */
-const viewerConfig = {
-    /* Allowed possible values are "FIT_PAGE", "FIT_WIDTH", "TWO_COLUMN", "TWO_COLUMN_FIT_PAGE" or "". */
-    defaultViewMode: "FIT_WIDTH",
-};
+// /* Control the default view mode */
+// const viewerConfig = {
+//     /* Allowed possible values are "FIT_PAGE", "FIT_WIDTH", "TWO_COLUMN", "TWO_COLUMN_FIT_PAGE" or "". */
+//     defaultViewMode: "FIT_WIDTH",
+// };
 
-// Function to get URL parameters
-function getQueryParam(param) {
-    let urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(param);
-}
-/* Wait for Adobe Acrobat Services PDF Embed API to be ready */
-document.addEventListener("adobe_dc_view_sdk.ready", function () {
-    /* Initialize the AdobeDC View object */
-    var adobeDCView = new AdobeDC.View({
-        /* Pass your registered client id */
-        clientId: "bea5c184d36e4ce297265b750571176e",
-        /* Pass the div id in which PDF should be rendered */
-        divId: "adobe-dc-view",
-    });
+// // Function to get URL parameters
+// function getQueryParam(param) {
+//     let urlParams = new URLSearchParams(window.location.search);
+//     return urlParams.get(param);
+// }
+// /* Wait for Adobe Acrobat Services PDF Embed API to be ready */
+// document.addEventListener("adobe_dc_view_sdk.ready", function () {
+//     /* Initialize the AdobeDC View object */
+//     var adobeDCView = new AdobeDC.View({
+//         /* Pass your registered client id */
+//         clientId: "bea5c184d36e4ce297265b750571176e",
+//         /* Pass the div id in which PDF should be rendered */
+//         divId: "adobe-dc-view",
+//     });
 
-    var pdfUrl = getQueryParam('pdfUrl'); // Default URL if parameter is not provided
+//     var pdfUrl = getQueryParam('pdfUrl'); // Default URL if parameter is not provided
             
 
-    /* Invoke the file preview API on Adobe DC View object */
-    adobeDCView.previewFile({
-        /* Pass information on how to access the file */
-        content: {
-            /* Location of file where it is hosted */
-            location: {
-                url: pdfUrl,
-                /*
-                If the file URL requires some additional headers, then it can be passed as follows:-
-                headers: [
-                    {
-                        key: "<HEADER_KEY>",
-                        value: "<HEADER_VALUE>",
-                    }
-                ]
-                */
-            },
-        },
-        /* Pass meta data of file */
-        metaData: {
-            /* file name */
-            fileName: "Bodea Brochure.pdf"
-        }
-    }, viewerConfig);
-});
+//     /* Invoke the file preview API on Adobe DC View object */
+//     adobeDCView.previewFile({
+//         /* Pass information on how to access the file */
+//         content: {
+//             /* Location of file where it is hosted */
+//             location: {
+//                 url: pdfUrl,
+//                 /*
+//                 If the file URL requires some additional headers, then it can be passed as follows:-
+//                 headers: [
+//                     {
+//                         key: "<HEADER_KEY>",
+//                         value: "<HEADER_VALUE>",
+//                     }
+//                 ]
+//                 */
+//             },
+//         },
+//         /* Pass meta data of file */
+//         metaData: {
+//             /* file name */
+//             fileName: "Bodea Brochure.pdf"
+//         }
+//     }, viewerConfig);
+// });
